e2b9e0813c24ed06f89afdf3f9a82680eef3cb35
Jack, Gao <rgao@litle.com>
2012-03-15 17:08:50 -0400

    functional tests capture and captureGivenAuth

--- test/functional/com/litle/sdk/TestCapture.java
+++ test/functional/com/litle/sdk/TestCapture.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import com.litle.sdk.generate.Capture;
 import com.litle.sdk.generate.CaptureResponse;
+import com.litle.sdk.generate.EnhancedData;
 
 public class TestCapture {
 
@@ -21,6 +22,37 @@ public class TestCapture {
 		CaptureResponse response = new LitleOnline().capture(capture);
 		assertEquals("Approved", response.getMessage());
 	}
+	
+	@Test
+	public void simpleCaptureWithPartial() throws Exception{
+		Capture capture = new Capture();
+		capture.setLitleTxnId(123456000);
+		capture.setAmount(BigInteger.valueOf(106L));
+		capture.setPartial(true);
+		capture.setPayPalNotes("Notes");
+		
+		CaptureResponse response = new LitleOnline().capture(capture);
+		assertEquals("Approved", response.getMessage());
+	}
+	
+	@Test
+	public void complexCapture() throws Exception{
+		Capture capture = new Capture();
+		capture.setLitleTxnId(123456000);
+		capture.setAmount(BigInteger.valueOf(106L));
+		capture.setPayPalNotes("Notes");
+		EnhancedData enhancedData = new EnhancedData();
+		enhancedData.setCustomerReference("Litle");
+		enhancedData.setSalesTax(BigInteger.valueOf(50L));
+		enhancedData.setDeliveryType("TBD");
+		capture.setEnhancedData(enhancedData);
+		capture.setPayPalOrderComplete(true);
+		
+		CaptureResponse response = new LitleOnline().capture(capture);
+		assertEquals("Approved", response.getMessage());
+	}
 
 
 }
+
+
--- test/functional/com/litle/sdk/TestCaptureGivenAuth.java
+++ test/functional/com/litle/sdk/TestCaptureGivenAuth.java
@@ -10,7 +10,14 @@ import org.junit.Test;
 import com.litle.sdk.generate.AuthInformation;
 import com.litle.sdk.generate.CaptureGivenAuth;
 import com.litle.sdk.generate.CaptureGivenAuthResponse;
+import com.litle.sdk.generate.CardTokenType;
 import com.litle.sdk.generate.CardType;
+import com.litle.sdk.generate.Contact;
+import com.litle.sdk.generate.FraudResult;
+import com.litle.sdk.generate.ProcessingInstructions;
+import com.sun.msv.datatype.xsd.ConcreteType;
+import com.sun.msv.datatype.xsd.TokenType;
+import com.sun.xml.xsom.impl.scd.Token;
 
 
 public class TestCaptureGivenAuth {
@@ -36,5 +43,87 @@ public class TestCaptureGivenAuth {
 		CaptureGivenAuthResponse response = new LitleOnline().captureGivenAuth(capturegivenauth);
 		assertEquals("Approved", response.getMessage());
 	}
+	
+	@Test
+	public void simpleCaptureGivenAuthWithToken() throws Exception{
+		CaptureGivenAuth capturegivenauth = new CaptureGivenAuth();
+		capturegivenauth.setAmount(BigInteger.valueOf(106L));
+		capturegivenauth.setOrderId("12344");
+		AuthInformation authInfo = new AuthInformation();
+		Calendar authDate = Calendar.getInstance();
+		authDate.set(2002, Calendar.OCTOBER, 9);
+		authInfo.setAuthDate(authDate);
+		authInfo.setAuthCode("543216");
+		authInfo.setAuthAmount(BigInteger.valueOf(12345L));
+		capturegivenauth.setAuthInformation(authInfo);
+		capturegivenauth.setOrderSource("ecommerce");
+		CardTokenType cardtoken = new CardTokenType();
+		cardtoken.setLitleToken("123456789101112");
+		cardtoken.setExpDate("1210");
+		cardtoken.setCardValidationNum("555");
+		cardtoken.setType("VI");
+		capturegivenauth.setToken(cardtoken);
+		CaptureGivenAuthResponse response = new LitleOnline().captureGivenAuth(capturegivenauth);
+		assertEquals("Approved", response.getMessage());
+	}
+	
+	@Test
+	public void complexCaptureGivenAuth() throws Exception{
+		CaptureGivenAuth capturegivenauth = new CaptureGivenAuth();
+		capturegivenauth.setAmount(BigInteger.valueOf(106L));
+		capturegivenauth.setOrderId("12344");
+		AuthInformation authInfo = new AuthInformation();
+		Calendar authDate = Calendar.getInstance();
+		authDate.set(2002, Calendar.OCTOBER, 9);
+		authInfo.setAuthDate(authDate);
+		authInfo.setAuthCode("543216");
+		authInfo.setAuthAmount(BigInteger.valueOf(12345L));
+		capturegivenauth.setAuthInformation(authInfo);
+		Contact contact = new Contact();
+		contact.setName("Bob");
+		contact.setCity("lowell");
+		contact.setState("MA");
+		contact.setEmail("litle.com");
+		capturegivenauth.setBillToAddress(contact);
+		ProcessingInstructions processinginstructions = new ProcessingInstructions();
+		processinginstructions.setBypassVelocityCheck(true);
+		capturegivenauth.setProcessingInstructions(processinginstructions);
+		capturegivenauth.setOrderSource("ecommerce");
+		CardType card = new CardType();
+		card.setType("VI");
+		card.setNumber("4100000000000001");
+		card.setExpDate("1210");
+		capturegivenauth.setCard(card);
+		CaptureGivenAuthResponse response = new LitleOnline().captureGivenAuth(capturegivenauth);
+		assertEquals("Approved", response.getMessage());
+	}
+	@Test
+	public void authInfo() throws Exception{
+		CaptureGivenAuth capturegivenauth = new CaptureGivenAuth();
+		capturegivenauth.setAmount(BigInteger.valueOf(106L));
+		capturegivenauth.setOrderId("12344");
+		AuthInformation authInfo = new AuthInformation();
+		Calendar authDate = Calendar.getInstance();
+		authDate.set(2002, Calendar.OCTOBER, 9);
+		authInfo.setAuthDate(authDate);
+		authInfo.setAuthCode("543216");
+		authInfo.setAuthAmount(BigInteger.valueOf(12345L));
+		FraudResult fraudresult = new FraudResult();
+		fraudresult.setAvsResult("12");
+		fraudresult.setCardValidationResult("123");
+		fraudresult.setAuthenticationResult("1");
+		fraudresult.setAdvancedAVSResult("123");
+		authInfo.setFraudResult(fraudresult);
+		capturegivenauth.setAuthInformation(authInfo);
+		capturegivenauth.setOrderSource("ecommerce");
+		CardType card = new CardType();
+		card.setType("VI");
+		card.setNumber("4100000000000001");
+		card.setExpDate("1210");
+		capturegivenauth.setCard(card);
+		CaptureGivenAuthResponse response = new LitleOnline().captureGivenAuth(capturegivenauth);
+		assertEquals("Approved", response.getMessage());
+	}
 
 }
+
