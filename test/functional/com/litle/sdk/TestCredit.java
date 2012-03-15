aaa3cbb3977a46f842910fc37795bd293b382299
Jack, Gao <rgao@litle.com>
2012-03-15 17:58:05 -0400

    functional tests credit

--- test/functional/com/litle/sdk/TestCredit.java
+++ test/functional/com/litle/sdk/TestCredit.java
@@ -18,7 +18,10 @@ import com.litle.sdk.generate.CaptureGivenAuth;
 import com.litle.sdk.generate.CaptureGivenAuthResponse;
 import com.litle.sdk.generate.CardType;
 import com.litle.sdk.generate.Credit;
+import com.litle.sdk.generate.Credit.Paypal;
 import com.litle.sdk.generate.CreditResponse;
+import com.litle.sdk.generate.PayPal;
+import com.litle.sdk.generate.ProcessingInstructions;
 
 public class TestCredit {
 
@@ -36,7 +39,55 @@ public class TestCredit {
 		CreditResponse response = new LitleOnline().credit(credit);
 		assertEquals("Approved", response.getMessage());
 	}
+	
+	@Test
+	public void simpleCreditWithPaypal() throws Exception{
+		Credit credit = new Credit();
+		credit.setAmount(BigInteger.valueOf(106L));
+		credit.setOrderId("123456");
+		credit.setOrderSource("ecommerce");
+		Paypal paypal = new Paypal();
+		paypal.setPayerId("1234");
+		credit.setPaypal(paypal);
+		CreditResponse response = new LitleOnline().credit(credit);
+		assertEquals("Approved", response.getMessage());
+	}
+	
+	@Test
+	public void paypalNotes() throws Exception{
+		Credit credit = new Credit();
+		credit.setAmount(BigInteger.valueOf(106L));
+		credit.setOrderId("12344");
+		credit.setPayPalNotes("Hello");
+		credit.setOrderSource("ecommerce");
+		CardType card = new CardType();
+		card.setType("VI");
+		card.setNumber("4100000000000001");
+		card.setExpDate("1210");
+		credit.setCard(card);
+		CreditResponse response = new LitleOnline().credit(credit);
+		assertEquals("Approved", response.getMessage());
+	}
+	
+	@Test
+	public void processingInstructionAndAmexData() throws Exception{
+		Credit credit = new Credit();
+		credit.setAmount(BigInteger.valueOf(2000L));
+		credit.setOrderId("12344");
+		credit.setOrderSource("ecommerce");
+		ProcessingInstructions processinginstructions = new ProcessingInstructions();
+		processinginstructions.setBypassVelocityCheck(true);
+		credit.setProcessingInstructions(processinginstructions);
+		CardType card = new CardType();
+		card.setType("VI");
+		card.setNumber("4100000000000001");
+		card.setExpDate("1210");
+		credit.setCard(card);
+		CreditResponse response = new LitleOnline().credit(credit);
+		assertEquals("Approved", response.getMessage());
+	}
 
 }
+
 
 
