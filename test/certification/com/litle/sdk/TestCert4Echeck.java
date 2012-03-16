cf9428cbfa1481a0c217acd0725c62240e488c5e
Gregory,Dake <gdake@litle.com>
2012-03-16 09:14:23 -0400

    pre-work on cert tests to provide a static LitleOnline for the rest of the test methods

--- test/certification/com/litle/sdk/TestCert4Echeck.java
+++ test/certification/com/litle/sdk/TestCert4Echeck.java
@@ -3,10 +3,18 @@ package com.litle.sdk;
 
 import static org.junit.Assert.fail;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestCert4Echeck {
 
+	private static LitleOnline litle;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		litle = new LitleOnline();
+	}
+	
 	@Test
 	public void test() {
 		fail("Implement me");
