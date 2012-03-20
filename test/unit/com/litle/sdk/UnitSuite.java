c9a73b7325d3576bf139e868899c876a93cb2d83
Gregory,Dake <gdake@litle.com>
2012-03-20 14:38:36 -0400

    fixing up classpath to remove duplicate entries for mockito in eclipse and adding a unit suite

---
+++ test/unit/com/litle/sdk/UnitSuite.java
@@ -0,0 +1,12 @@
+package com.litle.sdk;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestEnumerations.class,
+	TestLitleOnline.class
+})
+public class UnitSuite {
+}
