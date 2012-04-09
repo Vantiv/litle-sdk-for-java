7d1baa24553ca12fa7f86d5d291ae6ea5544ba6e
Gregory,Dake <gdake@litle.com>
2012-04-09 12:37:28 -0400

    fix problem with config file path on windows, and bump to 8.10.1

--- src/com/litle/sdk/Configuration.java
+++ src/com/litle/sdk/Configuration.java
@@ -7,7 +7,7 @@ public class Configuration {
 	private static final String LITLE_SDK_CONFIG = ".litle_SDK_config.properties";
 
 	public static File location() {
-		File file = new File(System.getenv("HOME") + File.separator + LITLE_SDK_CONFIG);
+		File file = new File(System.getProperty("user.home") + File.separator + LITLE_SDK_CONFIG);
 		if(System.getenv("LITLE_CONFIG_DIR") != null) {
 			file = new File(System.getenv("LITLE_CONFIG_DIR") + File.separator + LITLE_SDK_CONFIG);
 		}
