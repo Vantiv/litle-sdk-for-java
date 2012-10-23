0625590e41a8e3d149785f5e206d1a507c572a06
Gregory,Dake <gdake@litle.com>
2012-10-23 16:09:18 -0400

    Adding new files needed in 8.14.0

---
+++ test/functional/com/litle/sdk/TestUpdateCardValidationNumOnToken.java
@@ -0,0 +1,29 @@
+package com.litle.sdk;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.litle.sdk.generate.UpdateCardValidationNumOnToken;
+import com.litle.sdk.generate.UpdateCardValidationNumOnTokenResponse;
+
+public class TestUpdateCardValidationNumOnToken {
+
+	private static LitleOnline litle;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		litle = new LitleOnline();
+	}
+	
+	@Test
+	public void simple() throws Exception {
+		UpdateCardValidationNumOnToken update = new UpdateCardValidationNumOnToken();
+		update.setLitleToken("1111222233334444");
+		update.setCardValidationNum("123");
+		
+		UpdateCardValidationNumOnTokenResponse response = litle.updateCardValidationNumOnToken(update);
+		assertEquals(response.getMessage(), "805",response.getResponse());
+	}
+}
