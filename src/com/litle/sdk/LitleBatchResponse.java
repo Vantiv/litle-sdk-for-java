42f8f94fc0c41b23336775583a460841cb6edcc7
ramyavijay <ramyaraman.ssn@gmail.com>
2013-04-10 16:46:47 -0400

    adding functions to handle responses

--- src/com/litle/sdk/LitleBatchResponse.java
+++ src/com/litle/sdk/LitleBatchResponse.java
@@ -3,6 +3,7 @@ package com.litle.sdk;
 import java.io.StringReader;
 import java.math.BigInteger;
 
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.datatype.XMLGregorianCalendar;
 
@@ -13,10 +14,13 @@ import com.litle.sdk.generate.BatchResponse;
 import com.litle.sdk.generate.BillMeLaterResponseData;
 import com.litle.sdk.generate.FraudResult;
 import com.litle.sdk.generate.LitleOnlineResponse;
+import com.litle.sdk.generate.LitleRequest;
+import com.litle.sdk.generate.LitleResponse;
 import com.litle.sdk.generate.MethodOfPaymentTypeEnum;
 import com.litle.sdk.generate.Sale;
 import com.litle.sdk.generate.SaleResponse;
 import com.litle.sdk.generate.TransactionType;
+import com.litle.sdk.generate.TransactionTypeWithReportGroup;
 
 public class LitleBatchResponse {
 	private BatchResponse batchResponse;
@@ -33,44 +37,46 @@ public class LitleBatchResponse {
 //		}
 //	}
 	
-	public TransactionCodeEnum addTransactionToResponse(TransactionType transactionType) {
+	public TransactionCodeEnum addTransactionToResponse(TransactionTypeWithReportGroup transactionType) {
 		//Adding 1 to the number of transaction. This is on the assumption that we are adding one transaction to the batch at a time.
 		boolean transactionAdded = false;
+		LitleResponse litleResponse = new LitleResponse();
+		
 		
 		batchResponse.setId("batchId");
 		batchResponse.setLitleBatchId(0L);
 		batchResponse.setMerchantId("");
 		
-		if(transactionType instanceof Sale) {
+		if(transactionType instanceof SaleResponse) {
 			
 			SaleResponse saleResponse = new SaleResponse();
-			saleResponse.setId("");
-			saleResponse.setReportGroup("");
-			saleResponse.setLitleTxnId(0L);
-			saleResponse.setOrderId("");
-			saleResponse.setResponse("");
+			saleResponse.setId(transactionType.getId());
+			saleResponse.setReportGroup(((Sale) transactionType).getReportGroup());
+			saleResponse.setLitleTxnId(((Sale) transactionType).getLitleTxnId());
+			saleResponse.setOrderId(((Sale) transactionType).getOrderId());
+			saleResponse.setResponse(litleResponse.getResponse());
 			//saleResponse.setResponseTime(new XMLGregorianCalendar());
-			saleResponse.setMessage("");
-			saleResponse.setAuthCode("");
+			saleResponse.setMessage(litleResponse.getMessage());
+			//saleResponse.setAuthCode());
 			AccountInfoType accountInfoType = new AccountInfoType();
-			accountInfoType.setNumber("");
+			accountInfoType.setNumber(((Sale) transactionType).getCard().getNumber());
 			accountInfoType.setType(MethodOfPaymentTypeEnum.BL);
 			saleResponse.setAccountInformation(accountInfoType);
 			saleResponse.setBillMeLaterResponseData(new BillMeLaterResponseData());
 			transactionAdded = true;
 		}
-		else if(transactionType instanceof Authorization) {
+		else if(transactionType instanceof AuthorizationResponse) {
 			AuthorizationResponse authorizationResponse = new AuthorizationResponse();
-			authorizationResponse.setId("");
-			authorizationResponse.setReportGroup("");
-			authorizationResponse.setLitleTxnId(0L);
-			authorizationResponse.setOrderId("");
-			authorizationResponse.setResponse("");
+			authorizationResponse.setId(transactionType.getId());
+			authorizationResponse.setReportGroup(((Sale) transactionType).getReportGroup());
+			authorizationResponse.setLitleTxnId(((Sale) transactionType).getLitleTxnId());
+			authorizationResponse.setOrderId(litleResponse.getResponse());
+			authorizationResponse.setResponse(litleResponse.getResponse());
 			//authorizationResponse.setResponseTime(value);
-			authorizationResponse.setMessage("");
+			authorizationResponse.setMessage(litleResponse.getMessage());
 			authorizationResponse.setAuthCode("");
 			AccountInfoType accountInfoType = new AccountInfoType();
-			accountInfoType.setNumber("");
+			accountInfoType.setNumber(((Sale) transactionType).getCard().getNumber());
 			accountInfoType.setType(MethodOfPaymentTypeEnum.BL);
 			authorizationResponse.setAccountInformation(accountInfoType);
 			FraudResult fraudResult = new FraudResult();
@@ -93,5 +99,10 @@ public class LitleBatchResponse {
 	
 	public int getNumberOfTransactions(){
 		return this.batchResponse.getTransactionResponses().size();
+	}
+	
+	public TransactionTypeWithReportGroup returnTransactionTypeWithReportGroup() {
+		TransactionTypeWithReportGroup transReportGroup = new TransactionTypeWithReportGroup();
+		return transReportGroup;
 	}
 }
