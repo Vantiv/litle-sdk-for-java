a3533e367fd22636cbe4da9417a234879c4c7095
ramyavijay <ramyaraman.ssn@gmail.com>
2013-04-10 16:46:28 -0400

    Added functions to handle responses

--- src/com/litle/sdk/LitleBatchFileResponse.java
+++ src/com/litle/sdk/LitleBatchFileResponse.java
@@ -1,24 +1,34 @@
 package com.litle.sdk;
 
 import java.io.File;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import com.litle.sdk.generate.BatchResponse;
+import com.litle.sdk.generate.LitleOnlineResponse;
 import com.litle.sdk.generate.LitleResponse;
+import com.litle.sdk.generate.TransactionTypeWithReportGroup;
+import com.litle.sdk.generate.UpdateCardValidationNumOnTokenResponse;
 
 public class LitleBatchFileResponse {
 	private LitleResponse litleResponse;
-	private List<LitleBatchResponse> batchResponseList;
+	private List<BatchResponse> batchResponseList;
+	private Unmarshaller unmarshaller;
 	private File xmlFile;
 	
 	public LitleBatchFileResponse(File xmlFile){
 		// convert from xml to objects
 		this.litleResponse = new LitleResponse();
-		this.batchResponseList = new ArrayList<LitleBatchResponse>();
+		this.batchResponseList = new ArrayList<BatchResponse>();
 		this.xmlFile = xmlFile;
 	}
 	
-	public List<LitleBatchResponse> getBatchResponseList(){
+	public List<BatchResponse> getBatchResponseList(){
 		return batchResponseList;
 	}
 	
@@ -26,7 +36,20 @@ public class LitleBatchFileResponse {
 		return xmlFile;
 	}
 	
-	public void convertFileToObject() {
-		
+	public void convertFileToObject(String xmlResponseFile) throws JAXBException {
+		LitleBatchResponse litleBatchResponse = new LitleBatchResponse();
+		LitleResponse response = (LitleResponse)unmarshaller.unmarshal(new StringReader(xmlResponseFile));
+		for(int i = 0; i< response.getBatchResponses().size(); i++) {
+			batchResponseList.set(i, response.getBatchResponses().get(i));
+			for(int j=0; j< response.getBatchResponses().get(i).getTransactionResponses().size(); j++) {
+				litleBatchResponse.addTransactionToResponse(response.getBatchResponses().get(i).getTransactionResponses().get(j).getValue());
+			}
+		}
+//		LitleBatchResponse litleBatchResponse =  new LitleBatchResponse();
+//		for(int i=0; i< litleResponse.getBatchResponses().size(); i++) {
+//			litleBatchResponse.addTransactionToResponse();
+//			return (UpdateCardValidationNumOnTokenResponse)newresponse.getValue();
+//			
+//		}
 	}
 }
