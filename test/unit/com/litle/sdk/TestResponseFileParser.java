84d85f658f26418aa1da22a2dc365f4c333e3fb5
ramyavijay <ramyaraman.ssn@gmail.com>
2013-05-08 16:05:27 -0400

    adding unit test for response file parser

---
+++ test/unit/com/litle/sdk/TestResponseFileParser.java
@@ -0,0 +1,47 @@
+package com.litle.sdk;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+public class TestResponseFileParser {
+
+	private static ResponseFileParser responseFileParser;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		File fileToReturn = new File("test/unit/responseFolder/testFile.xml");
+		responseFileParser = new ResponseFileParser(fileToReturn);
+	}
+	
+	@Test
+    public void testgetNextTag() throws Exception {
+       
+		String retString = responseFileParser.getNextTag("litleResponse");
+		String outputString = "<litleResponse version=\"8.18\" xmlns=\"http://www.litle.com/schema\" response=\"0\" message=\"Valid Format\" litleSessionId=\"82822095378759694\"></litleResponse>";
+        assertEquals (outputString,retString);
+        retString = responseFileParser.getNextTag("batchResponse");
+        outputString = "<batchResponse litleBatchId=\"82822095378759702\" merchantId=\"101\"></batchResponse>";
+        assertEquals(outputString,retString);
+        
+        //TODO Check the values returned for saleResponse and AuthorizationResponse
+    }
+	
+	@Test
+	public void testOkToStartRecordingString() {
+		assertFalse(responseFileParser.okToStartRecordingString("litleResponse", "saleResponse"));
+		assertTrue(responseFileParser.okToStartRecordingString("SaleResponse", "saleResponse"));
+	}
+	
+	@Test
+	public void testokToStopRecordingString() {
+		assertTrue(responseFileParser.okToStopRecordingString("saleResponse", "</saleResponse>"));
+		assertFalse(responseFileParser.okToStopRecordingString("litleResponse", "saleResponse"));
+		assertFalse(responseFileParser.okToStopRecordingString("SaleResponse", "<saleResponse>"));
+	}
+}
