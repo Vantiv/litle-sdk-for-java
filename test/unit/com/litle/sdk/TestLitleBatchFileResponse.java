cdeb641e2106c6aa834434744d120cba5f9b393a
ramyavijay <ramyaraman.ssn@gmail.com>
2013-05-08 17:17:02 -0400

    started making changes after memory optimization

--- test/unit/com/litle/sdk/TestLitleBatchFileResponse.java
+++ test/unit/com/litle/sdk/TestLitleBatchFileResponse.java
@@ -21,37 +21,44 @@ public class TestLitleBatchFileResponse {
 	private static LitleBatchFileResponse litleBatchFileResponse;
 	File file;
 	
-	@Before
+//	@Before
 	public void before() throws Exception {
 		file = new File("test/unit/responseFolder/testFile.xml");
 		litleBatchFileResponse = new LitleBatchFileResponse(file);
 	}
 	
-	@Test
-	public void testgetBatchResponseList() throws Exception {
-		List<LitleBatchResponse> litleBatchResponseList = new ArrayList<LitleBatchResponse>();
-		litleBatchResponseList = litleBatchFileResponse.getBatchResponseList();
-	    assertEquals(1, litleBatchResponseList.size());
-		LitleBatchResponse batchResponse1 = litleBatchResponseList.get(0);
-		assertEquals("101",batchResponse1.getBatchResponse().getMerchantId());
-		LitleBatchResponse.TransactionTypeIterator it = batchResponse1.getTransactionResponses();
-		TransactionTypeWithReportGroup txn1 = it.next();
-		// TODO - verify some txn fields
-		TransactionTypeWithReportGroup txn2 = it.next();
-	}
+//	@Test
+//	public void testgetBatchResponseList() throws Exception {
+//		List<LitleBatchResponse> litleBatchResponseList = new ArrayList<LitleBatchResponse>();
+//		litleBatchResponseList = litleBatchFileResponse.getBatchResponseList();
+//	    assertEquals(1, litleBatchResponseList.size());
+//		LitleBatchResponse batchResponse1 = litleBatchResponseList.get(0);
+//		assertEquals("101",batchResponse1.getBatchResponse().getMerchantId());
+//		LitleBatchResponse.TransactionTypeIterator it = batchResponse1.getTransactionResponses();
+//		TransactionTypeWithReportGroup txn1 = it.next();
+//		// TODO - verify some txn fields
+//		TransactionTypeWithReportGroup txn2 = it.next();
+//	}
+	
+//	@Test
+//	public void testConstructorWithString() throws Exception {
+//		List<LitleBatchResponse> litleBatchResponseList = new ArrayList<LitleBatchResponse>();
+//		String xmlResponseString = new Scanner(new File("test/unit/responseFolder/testFile.xml")).useDelimiter("\\Z").next();
+//		litleBatchFileResponse = new LitleBatchFileResponse(xmlResponseString);
+//		litleBatchResponseList = litleBatchFileResponse.getBatchResponseList();
+//	    assertEquals(1, litleBatchResponseList.size());
+//	    // TODO - refactor me
+//		for(LitleBatchResponse list: litleBatchResponseList) {
+//			assertEquals("101",list.getBatchResponse().getMerchantId());
+////			assertEquals(2,list.getNumberOfTransactions());
+//		}
+//		
+//	}
 	
 	@Test
-	public void testConstructorWithString() throws Exception {
-		List<LitleBatchResponse> litleBatchResponseList = new ArrayList<LitleBatchResponse>();
-		String xmlResponseString = new Scanner(new File("test/unit/responseFolder/testFile.xml")).useDelimiter("\\Z").next();
-		litleBatchFileResponse = new LitleBatchFileResponse(xmlResponseString);
-		litleBatchResponseList = litleBatchFileResponse.getBatchResponseList();
-	    assertEquals(1, litleBatchResponseList.size());
-	    // TODO - refactor me
-		for(LitleBatchResponse list: litleBatchResponseList) {
-			assertEquals("101",list.getBatchResponse().getMerchantId());
-//			assertEquals(2,list.getNumberOfTransactions());
-		}
+	public void testGetNextLitleBatchResponse() {
+		LitleBatchResponse litleBatchResponse = litleBatchFileResponse.getNextLitleBatchResponse();
+		assertEquals(litleBatchResponse.getMerchantId(), "101");
 		
 	}
 }
