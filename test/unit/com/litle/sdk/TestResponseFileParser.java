9a3a9c0a136931d730dd56faa875e6a7cb29d977
ramyavijay <ramyaraman.ssn@gmail.com>
2013-05-10 12:28:26 -0400

    verifying all other possible if-else conditions on parser code

--- test/unit/com/litle/sdk/TestResponseFileParser.java
+++ test/unit/com/litle/sdk/TestResponseFileParser.java
@@ -26,27 +26,36 @@ public class TestResponseFileParser {
 		String outputString = "<litleResponse version=\"8.18\" xmlns=\"http://www.litle.com/schema\" response=\"0\" message=\"Valid Format\" litleSessionId=\"82822223274065939\"></litleResponse>";
         assertEquals (outputString,retString);
         retString = responseFileParser.getNextTag("batchResponse");
-        outputString = "<batchResponse litleBatchId=\"82822223274065947\" merchantId=\"101\"></batchResponse>";
+        outputString = "<batchResponse xmlns=\"http://www.litle.com/schema\" litleBatchId=\"82822223274065947\" merchantId=\"101\"></batchResponse>";
         assertEquals(outputString,retString);
 		retString = responseFileParser.getNextTag("transactionResponse");
         retString = responseFileParser.getNextTag("transactionResponse");
+        //verifyException - The test response file has only 2 transactions in the first batch
+        try {
+        	retString = responseFileParser.getNextTag("transactionResponse");
+        }catch(Exception e) {
+        	assertEquals("All payments in this batch have already been retrieved.", e.getMessage());
+        }
+
         retString = responseFileParser.getNextTag("batchResponse");
-        outputString = "<batchResponse litleBatchId=\"82822223274065954\" merchantId=\"101\"></batchResponse>";
+        outputString = "<batchResponse xmlns=\"http://www.litle.com/schema\" litleBatchId=\"82822223274065954\" merchantId=\"101\"></batchResponse>";
         assertEquals(outputString,retString);
         retString = responseFileParser.getNextTag("transactionResponse");
-        
-        //TODO Check the values returned for saleResponse and AuthorizationResponse
+
     }
 	
 	@Test
 	public void testOkToStartRecordingString() {
 		assertFalse(responseFileParser.okToStartRecordingString("litleResponse", "saleResponse"));
 		assertTrue(responseFileParser.okToStartRecordingString("SaleResponse", "saleResponse"));
+		assertTrue(responseFileParser.okToStartRecordingString("<transactionResponse", "<saleResponse"));
 	}
 	
 	@Test
 	public void testokToStopRecordingString() {
-		//assertTrue(responseFileParser.okToStopRecordingString("saleResponse", "</saleResponse>"));
+		assertTrue(responseFileParser.okToStopRecordingString("</transactionResponse>", "</saleResponse>"));
+		assertTrue(responseFileParser.okToStopRecordingString("</saleResponse>", "</saleResponse>"));
+		assertFalse(responseFileParser.okToStopRecordingString("<transactionResponse>", "</saleResponse>"));
 		assertFalse(responseFileParser.okToStopRecordingString("litleResponse", "saleResponse"));
 		assertFalse(responseFileParser.okToStopRecordingString("SaleResponse", "<saleResponse>"));
 	}
