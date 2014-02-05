4f84d7e906d9c8867f3c7b89f62c5b1d4b75c157
kook005 <fengxuzhong440@gmail.com>
2014-02-05 17:32:58 -0500

    update schema to 8.24

--- src/com/litle/sdk/Versions.java
+++ src/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="8.23";
-    public static final String SDK_VERSION="Java;8.23.1";
+    public static final String XML_VERSION="8.24";
+    public static final String SDK_VERSION="Java;8.24.0";
 
 }
