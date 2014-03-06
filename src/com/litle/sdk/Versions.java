38b4913b1bac958578c143da47cab337a3156c77
pshah91 <pshah91@ccs.neu.edu>
2014-03-06 13:16:52 -0500

    Version 8.25.0 (March 6,2014) * Feature: Added support for ROAM: Authorization, ForceCapture, CaptureGivenAuth, Sale, Credit

--- src/com/litle/sdk/Versions.java
+++ src/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="8.24";
-    public static final String SDK_VERSION="Java;8.24.0";
+    public static final String XML_VERSION="8.25";
+    public static final String SDK_VERSION="Java;8.25.0";
 
 }
