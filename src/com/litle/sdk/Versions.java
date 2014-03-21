1423fa069edbdfdd0514c31245f965a0427d3ead
Bill, Clements <bclement@bclement-vm1.dwi.litle.com>
2014-03-21 16:13:44 -0400

    == Version 8.25.1 (March 21, 2014) * Bugfix: delete the reports directory on clean in build.xml

--- src/com/litle/sdk/Versions.java
+++ src/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="8.25";
-    public static final String SDK_VERSION="Java;8.25.0";
+    public static final String SDK_VERSION="Java;8.25.1";
 
 }
