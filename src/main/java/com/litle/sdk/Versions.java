df30c0096ac096103788d9375135801b9c892204
SDK Development User - Dev <sdkdev@sdkdev-vm2.dwi.litle.com>
2015-03-09 10:38:54 -0400

    changed versions

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="9.3";
-    public static final String SDK_VERSION="Java;9.3.0";
+    public static final String SDK_VERSION="Java;9.3.1";
 
 }
