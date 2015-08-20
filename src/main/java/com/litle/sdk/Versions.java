ec1b18527849a51d753a76f91493f5ede98138c7
Amit <amitvig22@gmail.com>
2015-08-20 14:29:45 -0400

    Update Versions.java

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="8.29";
-    public static final String SDK_VERSION="Java;8.29.2";
+    public static final String SDK_VERSION="Java;8.29.3";
 
 }
