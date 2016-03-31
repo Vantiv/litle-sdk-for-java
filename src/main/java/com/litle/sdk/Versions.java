b48a39c628d31c52d70d007c2eb40882d72bedd4
TsilenT <stsai@litle.com>
2016-03-31 10:43:40 -0400

    9.9 - Pin support for gift cards

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="9.8";
-    public static final String SDK_VERSION="Java;9.8.0";
+    public static final String XML_VERSION="9.9";
+    public static final String SDK_VERSION="Java;9.9.0";
 
 }
