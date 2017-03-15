61b41cfdd2717d689abb042bc01f5628297c7360
Houzl <zhilonghou@gmail.com>
2017-03-15 16:15:38 -0400

    Rev to 9.12

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="9.10";
-    public static final String SDK_VERSION="Java;9.10.0";
+    public static final String XML_VERSION="9.12";
+    public static final String SDK_VERSION="Java;9.12.0";
 
 }
