7a5d498cb5ff0b7af3ad122dc88ec5c702cdae7d
Brian Arnold <sdksupport@litle.com>
2017-07-31 14:57:49 -0400

    Update Communication to use new httpclient patterns, add new Exception classes for Open access, bump version

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="11.0";
-    public static final String SDK_VERSION="Java;11.0.1";
+    public static final String SDK_VERSION="Java;11.0.2";
 
 }
