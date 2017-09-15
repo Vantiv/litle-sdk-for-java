6a2620cfda54b801ae5ff56452fb487af17161ad
brianarnold789 <sdksupport@vantiv.com>
2017-09-15 09:30:13 -0400

    update Java Doc for timeout

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="11.0";
-    public static final String SDK_VERSION="Java;11.0.2";
+    public static final String XML_VERSION="11.2";
+    public static final String SDK_VERSION="Java;11.2.2";
 
 }
