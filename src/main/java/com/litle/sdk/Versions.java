ba3a8087c63ad8db4ca09a933adc692e54f58368
brianarnold789 <brian.arnold@vantiv.com>
2017-10-29 01:43:10 -0400

    update minor version for release

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="11.2";
-    public static final String SDK_VERSION="Java;11.2.2";
+    public static final String SDK_VERSION="Java;11.2.1";
 
 }
