37b3f5b247bc0801203991669c9932c430433b0d
Riddhi,Chabhadia <chabhadia.r@husky.neu.edu>
2017-10-30 09:38:19 -0400

    changed timeout in communication.java

--- src/main/java/com/litle/sdk/Communication.java
+++ src/main/java/com/litle/sdk/Communication.java
@@ -105,11 +105,13 @@ public class Communication {
 			proxy = new HttpHost(proxyHost, Integer.valueOf(proxyPort));
 			requestConfig = RequestConfig.copy(RequestConfig.DEFAULT)
 					.setProxy(proxy)
-					.setConnectionRequestTimeout(httpTimeout)
+					.setSocketTimeout(httpTimeout)
+					.setConnectTimeout(httpTimeout)
 					.build();
 		} else {
 			requestConfig = RequestConfig.copy(RequestConfig.DEFAULT)
-					.setConnectionRequestTimeout(httpTimeout)
+			        .setSocketTimeout(httpTimeout)
+                    .setConnectTimeout(httpTimeout)
 					.build();
 		}
 
