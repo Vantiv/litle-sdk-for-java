0c27c3b77b26b13d6fabe1673317231d864fe25d
brianarnold789 <brian.arnold@vantiv.com>
2017-11-09 15:42:51 -0500

    update HTTP client version and Communications class

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="11.0";
-    public static final String SDK_VERSION="Java;11.0.0";
+    public static final String XML_VERSION="11.3";
+    public static final String SDK_VERSION="Java;11.3.0";
 
 }
