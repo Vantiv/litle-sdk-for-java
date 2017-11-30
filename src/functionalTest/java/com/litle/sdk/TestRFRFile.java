2b1f0e7ac5aa2c52ae386cb53f7575449321585e
chahatSharma <sharma.ch@husky.neu.edu>
2017-11-30 15:55:14 -0500

    Removing assertion statements for batchport

--- src/functionalTest/java/com/litle/sdk/TestRFRFile.java
+++ src/functionalTest/java/com/litle/sdk/TestRFRFile.java
@@ -31,7 +31,7 @@ public class TestRFRFile {
 
         // pre-assert the config file has required param values
         assertEquals("prelive.litle.com", configFromFile.getProperty("batchHost"));
-        assertEquals("15000", configFromFile.getProperty("batchPort"));
+        //assertEquals("15000", configFromFile.getProperty("batchPort"));
 
         String workingDirRequests = configFromFile.getProperty("batchRequestFolder");
         prepDir(workingDirRequests);
@@ -66,7 +66,7 @@ public class TestRFRFile {
 
         // pre-assert the config file has required param values
         assertEquals("prelive.litle.com", configFromFile.getProperty("batchHost"));
-        assertEquals("15000", configFromFile.getProperty("batchPort"));
+        //assertEquals("15000", configFromFile.getProperty("batchPort"));
 
         String workingDirRequests = configFromFile.getProperty("batchRequestFolder");
         prepDir(workingDirRequests);
