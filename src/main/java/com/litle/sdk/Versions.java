1d52dd1346f5e93577f962640d3a5a0ea7713d49
swapnilhande <swapnil.hande@gmail.com>
2018-03-15 17:19:02 -0400

    new release

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="11.3";
-    public static final String SDK_VERSION="Java;11.3.0";
+    public static final String SDK_VERSION="Java;11.3.1";
 
 }
