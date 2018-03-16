0dbbc726725125b3da8aa1a98ee79878fd19a38b
hvora <harshit.hvora@gmail.com>
2018-03-16 11:45:57 -0400

    updated xml to v8.31

--- src/functionalTest/java/com/litle/sdk/TestCaptureGivenAuth.java
+++ src/functionalTest/java/com/litle/sdk/TestCaptureGivenAuth.java
@@ -177,6 +177,29 @@ public class TestCaptureGivenAuth {
 		CaptureGivenAuthResponse response = litle.captureGivenAuth(capturegivenauth);
 		assertEquals("Approved", response.getMessage());
 	}
+
+	@Test
+	public void testCaptureGivenAuthWithProcessingTypeCardOnFile() throws Exception{
+		CaptureGivenAuth capturegivenauth = new CaptureGivenAuth();
+		capturegivenauth.setAmount(106L);
+		capturegivenauth.setOrderId("12344");
+		AuthInformation authInfo = new AuthInformation();
+		Calendar authDate = Calendar.getInstance();
+		authDate.set(2002, Calendar.OCTOBER, 9);
+		authInfo.setAuthDate(authDate);
+		authInfo.setAuthCode("543216");
+		authInfo.setAuthAmount(12345L);
+		capturegivenauth.setAuthInformation(authInfo);
+		capturegivenauth.setOrderSource(OrderSourceType.ECOMMERCE);
+		capturegivenauth.setProcessingType(ProcessingTypeEnum.INITIAL_COF);
+		CardType card = new CardType();
+		card.setType(MethodOfPaymentTypeEnum.VI);
+		card.setNumber("4100000000000000");
+		card.setExpDate("1210");
+		capturegivenauth.setCard(card);
+		CaptureGivenAuthResponse response = litle.captureGivenAuth(capturegivenauth);
+		assertEquals("Approved", response.getMessage());
+	}
 	
 	@Test
 	public void testCaptureGivenAuthWithOrigNetworkTxnIdandAmount() throws Exception{
--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="8.30";
-    public static final String SDK_VERSION="Java;8.30.0";
+    public static final String XML_VERSION="8.31";
+    public static final String SDK_VERSION="Java;8.31.0";
 
 }
