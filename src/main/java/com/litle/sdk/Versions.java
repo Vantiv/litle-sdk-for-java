bf03ac5eac17bad3d19147c8e0f541f04a8702e3
hvora <harshit.hvora@gmail.com>
2018-03-20 15:41:11 -0400

    upgraded xml version to 10.8

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -2,7 +2,7 @@ package com.litle.sdk;
 
 public class Versions {
 
-    public static final String XML_VERSION="10.5";
-    public static final String SDK_VERSION="Java;10.5.3";
+    public static final String XML_VERSION="10.8";
+    public static final String SDK_VERSION="Java;10.8.0";
 
 }
