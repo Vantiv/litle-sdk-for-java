1a55877bb511c68d52fca7e315a98f3003ba3de3
swapnilhande <swapnil.hande@gmail.com>
2018-04-24 13:45:46 -0400

    Updated release version

--- src/main/java/com/litle/sdk/Versions.java
+++ src/main/java/com/litle/sdk/Versions.java
@@ -3,6 +3,6 @@ package com.litle.sdk;
 public class Versions {
 
     public static final String XML_VERSION="9.14";
-    public static final String SDK_VERSION="Java;9.14.0";
+    public static final String SDK_VERSION="Java;9.14.1";
 
 }
