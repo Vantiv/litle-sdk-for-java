78bc9401cbfffb12ab21babaed120ebe504ba939
Azhur Viano -Development <AzhurV@users.noreply.github.com>
2020-03-25 11:39:45 -0400

    Change sample using sftp to not run if prelive is down

--- samples/other/src/main/java/RfrLitleExample.java
+++ samples/other/src/main/java/RfrLitleExample.java
@@ -5,6 +5,12 @@ import com.litle.sdk.generate.RFRRequest;
 import java.util.Calendar;
 public class RfrLitleExample {
     public static void main(String[] args) {
+        String preliveStatus = System.getenv("preliveStatus");
+
+        if(preliveStatus != null && preliveStatus.equalsIgnoreCase("down")){
+            return;
+        }
+        
         String merchantId = "0180";
         String requestFileName = "litleSdk-testRFRFile-fileConfigSFTP.xml";
         RFRRequest rfrRequest = new RFRRequest();
